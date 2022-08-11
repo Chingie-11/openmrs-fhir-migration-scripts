bde7189eb44b7f4920413a393b03464e157f89ef
Chingie-11 <fosterkasendajr@gmail.com>
2022-08-12 00:11:52 +0200

    Added Women health screening

--- src/careplan.js
+++ src/careplan.js
@@ -477,6 +477,13 @@ async function createTask() {
                 const dateThen = dayjs(details.birthDate)
                 let yearsDiff = currentDate.diff(dateThen, 'year')
                 let monthsDiff = currentDate.diff(dateThen, 'month')
+                let addWomenHealth = tasks.push({
+                    resource: womenHealth, "request": {
+                        "method": "POST",
+                        "url": "Task/"
+                    }
+                });
+
                 if (details.gender !== "male") {
 
 
@@ -496,47 +503,43 @@ async function createTask() {
                     } else if (yearsDiff < 13 && yearsDiff >= 9) {
                         womenHealth.reasonReference.reference = "Questionnaire/art-client-womens-health-screening-female-9-to-14-years"
                         vitals.reasonReference.reference = "Questionnaire/art-client-vitals-5-to-13-years"
+                        addWomenHealth
 
                     } else if (yearsDiff < 14 && yearsDiff >= 13) {
                         womenHealth.reasonReference.reference = "Questionnaire/art-client-womens-health-screening-female-9-to-14-years"
                         vitals.reasonReference.reference = "Questionnaire/art-client-vitals-13-to-15-years"
+                        addWomenHealth
 
                     } else if (yearsDiff < 15 && yearsDiff >= 14) {
                         vitals.reasonReference.reference = "Questionnaire/art-client-vitals-13-to-15-years"
+                        addWomenHealth
 
                     } else if (yearsDiff < 25 && yearsDiff >= 15) {
-
 
                         womenHealth.reasonReference.reference = "Questionnaire/art-client-womens-health-screening-female-15-to-25-years"
                         vitals.reasonReference.reference = "Questionnaire/art-client-vitals-female-15-to-30-years"
                         guardianUpdates.reasonReference.reference = "Questionnaire/patient-guardian-updates-15-years-plus"
+                        addWomenHealth
 
                     } else if (yearsDiff < 30 && yearsDiff >= 25) {
-
-
                         womenHealth.reasonReference.reference = "Questionnaire/art-client-womens-health-screening-female-25-years-plus"
                         vitals.reasonReference.reference = "Questionnaire/art-client-vitals-female-15-to-30-years"
                         guardianUpdates.reasonReference.reference = "Questionnaire/patient-guardian-updates-15-years-plus"
-
+                        addWomenHealth
+                        
                     } else if (yearsDiff < 40 && yearsDiff >= 30) {
                         womenHealth.reasonReference.reference = "Questionnaire/art-client-womens-health-screening-female-25-years-plus"
                         vitals.reasonReference.reference = "Questionnaire/art-client-vitals-female-30-to-40-years"
                         guardianUpdates.reasonReference.reference = "Questionnaire/patient-guardian-updates-15-years-plus"
+                        addWomenHealth
 
                     } else if (yearsDiff >= 40) {
                         womenHealth.reasonReference.reference = "Questionnaire/art-client-womens-health-screening-female-25-years-plus"
                         vitals.reasonReference.reference = "Questionnaire/art-client-vitals-female-40-years-plus"
                         guardianUpdates.reasonReference.reference = "Questionnaire/patient-guardian-updates-15-years-plus"
+                        addWomenHealth
                     }
-
-
-                    //womens health starts from 9 years, need to figure this one out.
-                    tasks.push({
-                        resource: womenHealth, "request": {
-                            "method": "POST",
-                            "url": "Task/"
-                        }
-                    });
+                  
 
                 } else {
 
